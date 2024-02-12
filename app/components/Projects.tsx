d1ad7e28dfceee0463c3b587beb6493de6066c50
Anders Boyum <acboyum@gmail.com>
2024-02-12 15:46:00 -0600

    missed a file

---
+++ app/components/Projects.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+export default function Projects() {
+    const exampleProjects = [
+        {name: "Momentum", description: "Gamified personal development app built for a client."},
+        {
+            name: "StuMP3d", description: "Music guessing game, perfect for long car rides and testing your friends!"
+        }
+    ]
+
+    return (
+        <div id="projectCard" className="w-40 h-40 border-white border-2 rounded-sm ">
+            
+        </div>
+    )
+}
