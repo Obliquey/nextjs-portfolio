8e4eac2b0f63395b59e15dfe3ce63e9ccd9055d6
Anders Boyum <acboyum@gmail.com>
2024-02-14 16:10:06 -0600

    reformattiiiiiing

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,46 @@
 import { Manuale } from "next/font/google";
 import Image from "next/image";
-import Nav from "./components/nav";
-// import Projects from "./components/projects";
 
 export default function Home() {
-  // const imgURL = "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29kaW5nfGVufDB8fDB8fHww";
-  const imgURL = "/pics/Random_Selfie.jpg";
   return (
     // for my portfolio / blog
     // BLOG WILL COME LATER
 
     // for now, just get the basics up:
       // 1. An About Me page, with something akin to a cover letter
-      // 2. A "History" type page with past work experiences
+      // 2. A "History" type page with past work experiences?
       // 3. A Projects page, with a current list of Github projects
-      // 4. An informations page, including a small box for visitors to send me emails.
+      // 4. An informations page, including a small box for visitors to send me emails?
       
-      <main className="">
-        <div id="parent" className="flex flex-row gap-12 place-content-center mt-10">
-            <div id="image" className="basis-1/5">
-                <img src={imgURL} alt="something about coding" className="rounded-lg shadow-lg"/>
-            </div>
-            <div id="bio" className="basis-2/5 flex flex-col">
-                <p className="text-white text-3xl place-self-center">
-                  Anders Boyum
-                </p>
-                <p className="text-white text-xl place-self-center">I'm a software developer </p>
-            </div>
+
+      // need to separate these elements out more, make their positioning less dependent on each other. 
+      <main className="flex flex-row place-content-center m-auto absolute top-1/3 inset-x-1/4 gap-10">
+        <div id="nametag" className="text-white text-8xl m-auto place-self-center">
+            <h1>anders <br></br>boyum</h1>
         </div>
-
-
-        {/* will need to figure out how best to print projects to the DOM. Will be dependent on how I retrieve that info from Github */}
-        {/* <div id="projects" className="flex flex-row gap-10 place-content-center mt-40">
-          <Projects />
-        </div> */}
-
+        <div id="pinstripe" className="text-white text-8xl place-self-center h-full">
+          <p>|</p>
+        </div>
+        <div id="bio" className="text-white text-6xl place-self-center">
+          <p>full-stack engineer</p>
+        </div>
       </main>
   );
 }
+
+
+// PREVIOUS BUILD
+{/* <main className="">
+<div id="parent" className="flex flex-row gap-12 place-content-center mt-10">
+    <div id="image" className="basis-1/5">
+        <img src={imgURL} alt="something about coding" className="rounded-lg shadow-lg"/>
+    </div>
+    <div id="bio" className="basis-2/5 flex flex-col">
+        <p className="text-white text-3xl place-self-center">
+          Anders Boyum
+        </p>
+        <p className="text-white text-xl place-self-center">I'm a software developer </p>
+    </div>
+</div>
+
+</main> */}
