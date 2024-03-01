a213302b9bbd130e088bb36580927d797bac6d4d
Anders Boyum <acboyum@gmail.com>
2024-03-01 12:45:41 -0600

    more or less figured out how to have my front page though I might add an image

--- app/page.tsx
+++ app/page.tsx
@@ -14,33 +14,18 @@ export default function Home() {
       
 
       // need to separate these elements out more, make their positioning less dependent on each other. 
-      <main className="flex flex-row place-content-center m-auto absolute top-1/3 inset-x-1/4 gap-10">
-        <div id="nametag" className="text-white text-8xl m-auto place-self-center">
-            <h1>anders <br></br>boyum</h1>
-        </div>
-        <div id="pinstripe" className="text-white text-8xl place-self-center h-full">
-          <p>|</p>
-        </div>
-        <div id="bio" className="text-white text-6xl place-self-center">
-          <p>full-stack engineer</p>
+      <main className="flex flex-row place-content-center m-auto absolute top-1/3 inset-x-1/4 gap-1">
+        <div id="nametag" className="flex flex-row place-content-center m-auto  top-1/3 inset-x-1/4 gap-1">
+          <div className="text-white text-8xl m-auto place-self-center">
+              <h1>anders <br></br>boyum</h1>
+          </div>
+          <div id="pinstripe" className="text-white text-9xl place-self-center h-full">
+            <p>|</p>
+          </div>
+          <div id="bio" className="text-white text-6xl place-self-center">
+            <p>full-stack engineer</p>
+          </div>
         </div>
       </main>
   );
 }
-
-
-// PREVIOUS BUILD
-{/* <main className="">
-<div id="parent" className="flex flex-row gap-12 place-content-center mt-10">
-    <div id="image" className="basis-1/5">
-        <img src={imgURL} alt="something about coding" className="rounded-lg shadow-lg"/>
-    </div>
-    <div id="bio" className="basis-2/5 flex flex-col">
-        <p className="text-white text-3xl place-self-center">
-          Anders Boyum
-        </p>
-        <p className="text-white text-xl place-self-center">I'm a software developer </p>
-    </div>
-</div>
-
-</main> */}
